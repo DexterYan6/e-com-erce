b929973d4836cb20dd1a3c99125d91929418f3da
Dexter Yan <maildexteryan@gmail.com>
2024-04-19 21:11:09 -0400

    Create Carousel

---
+++ app/components/CarouselComp.js
@@ -0,0 +1,22 @@
+"use client"
+
+import { Carousel } from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+
+export default function CarouselComp() {
+    return (
+        <div className="max-w-[1200px] mx-auto">
+            <Carousel showArrows={true} autoPlay={true} interval={3000} infiniteLoop={true} showThumbs={false}>
+                <div>
+                    <img src="/images/banner/banner1.jpg" />
+                </div>
+                <div>
+                    <img src="/images/banner/banner2.jpg" />
+                </div>
+                <div>
+                    <img src="/images/banner/banner3.jpg" />
+                </div>
+            </Carousel>
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 "use client"
 
 import MainLayout from './layouts/MainLayout'
+import CarouselComp from "./components/CarouselComp"
 
 export default function Home() {
   return (
     <MainLayout>
-      cool
+      <CarouselComp />
     </MainLayout>
   );
 }
